2f293109b28dfef84b5e277d326392d4725f2375
Vasco Lavos <vazkii@hotmail.com>
2017-09-16 21:34:50 +0100

    Commented out debug message

--- src/main/java/net/daveyx0/primitivemobs/spawn/PrimitiveMobsWorldSpawner.java
+++ src/main/java/net/daveyx0/primitivemobs/spawn/PrimitiveMobsWorldSpawner.java
@@ -130,7 +130,7 @@ public class PrimitiveMobsWorldSpawner
                                 {
                                     ++successCount;
 
-                                    if(entity instanceof EntityLilyLurker) {PrimitiveMobsLogger.PMlogger.info("Got here: " +" "+ entity.posX +" "+ entity.posY +" "+ entity.posZ);}
+//                                    if(entity instanceof EntityLilyLurker) {PrimitiveMobsLogger.PMlogger.info("Got here: " +" "+ entity.posX +" "+ entity.posY +" "+ entity.posZ);}
                                     worldServerIn.spawnEntity(entity);
                                 }
                                 else
